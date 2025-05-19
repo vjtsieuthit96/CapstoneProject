ec14e1b73e7536171b27bc4c3f44cbfdb016dc8d
Andy THR <trunghauphan2710@gmail.com>
2025-05-19 21:23:02 +0700

    update data

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: CapstoneProject
 - Unity version: Unity 6000.0.32f1
 - Active game object:
-  - Name: GunEffect
+  - Name: Guntest
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/_HauFolder/_Script/GunData.cs
@@ -0,0 +1,63 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GunData", menuName = "Scriptable Objects/GunData")]
+public class GunData : ScriptableObject
+{
+    public string GunName;
+    public float Weight;
+    public float Gunrecoil;
+    public float BulletSpeed;
+    public float RateofFire;
+    public float Range;
+    public float PhysicalDamage;
+    public MagicDamageEffect MagicEffect;
+    public Guntype Type;
+    public ShootingMethod Method
+    {
+        get
+        {
+            switch (Type)
+            {
+                case Guntype.Piston:
+                    return ShootingMethod.Single;
+                case Guntype.Rifle:
+                    return ShootingMethod.Both;
+                case Guntype.Submachine:
+                    return ShootingMethod.Automatic;
+                case Guntype.Sniper:
+                    return ShootingMethod.Single;
+                default: return ShootingMethod.Single;
+            }
+        }
+    }
+}
+
+
+public enum Guntype
+{
+    Piston,
+    Rifle,
+    Submachine,
+    Sniper
+}
+
+public enum ShootingMethod
+{
+    Single,
+    Automatic,
+    Both
+}
+
+public enum MagicDamageEffect
+{
+    None,
+    Fire, 
+    Ice,
+    Lightning,
+    Poison,
+    Dark,
+    Light,
+    DamageOverTime,
+    Stun,
+}
