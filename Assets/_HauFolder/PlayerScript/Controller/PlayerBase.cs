f4d775217b1c1229d668ad190276abe3d00dff5c
Andy THR <trunghauphan2710@gmail.com>
2025-05-31 14:03:39 +0700

    player update

--- Assets/_HauFolder/PlayerScript/Controller/PlayerBase.cs
+++ Assets/_HauFolder/PlayerScript/Controller/PlayerBase.cs
@@ -4,106 +4,106 @@ using UnityEngine;
 
 public class PlayerBase : MonoBehaviour
 {
-    // data
-    public PlayerData Data;
-    [SerializeField] private GunController Gun;
+    //// data
+    //public PlayerData Data;
+    //[SerializeField] private GunController Gun;
     
-    // interfaces
-    private ILookBehavior PlayerLook;
-    private PlayerStateManager StateManager;
-    private IInputSystem Input;
+    //// interfaces
+    //private ILookBehavior PlayerLook;
+    //private PlayerStateManager StateManager;
+    //private IInputSystem Input;
 
-    // References
-    private PlayerCore Core;
+    //// References
+    //private PlayerCore Core;
 
-    // base component
-    private Rigidbody rb;
-    private Transform PlayerTransform;
-    private GroundCheck groundCheck;
-    public Transform cameraTransform;
-    private CameraRecoil CamRecoil;
-    private IJumpable Jump;
-    // Coordinates
-    private float RotatonX;
-    private float RotatonY;
+    //// base component
+    //private Rigidbody rb;
+    //private Transform PlayerTransform;
+    //private GroundCheck groundCheck;
+    //public Transform cameraTransform;
+    //private CameraRecoil CamRecoil;
+    //private IJumpable Jump;
+    //// Coordinates
+    //private float RotatonX;
+    //private float RotatonY;
 
-    private void Awake()
-    {
-        CamRecoil = cameraTransform.GetComponent<CameraRecoil>();
-        PlayerTransform = GetComponent<Transform>();
-        rb = GetComponent<Rigidbody>();
-        PlayerLook = new PlayerLook();
-        groundCheck = GetComponent<GroundCheck>();
-        Input = new TestInputController();
-        Jump = new PlayerJump();
-        CameraBobbing camBobbing = cameraTransform.GetComponent<CameraBobbing>();
-        StateManager = new PlayerStateManager(groundCheck, Input, rb, Data, camBobbing);
-        Core = new PlayerCore(Input, StateManager, PlayerLook, rb, Data,Jump);
-        Core.Equip(Gun);
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+    //private void Awake()
+    //{
+    //    CamRecoil = cameraTransform.GetComponent<CameraRecoil>();
+    //    PlayerTransform = GetComponent<Transform>();
+    //    rb = GetComponent<Rigidbody>();
+    //    PlayerLook = new PlayerLook();
+    //    groundCheck = GetComponent<GroundCheck>();
+    //    Input = new TestInputController();
+    //    Jump = new PlayerJump();
+    //    CameraBobbing camBobbing = cameraTransform.GetComponent<CameraBobbing>();
+    //    StateManager = new PlayerStateManager(groundCheck, Input, rb, Data, camBobbing);
+    //    Core = new PlayerCore(Input, StateManager, PlayerLook, rb, Data,Jump);
+    //    Core.Equip(Gun);
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //}
 
-    private void Update()
-    {
-        StateManager.UpdateState();
-        PlayerMovement();
-        PlayerLookable();
-        PlayerShoot();
-        PlayerReload();
-        PlayerOnJump();
-    }
+    //private void Update()
+    //{
+    //    StateManager.UpdateState();
+    //    PlayerMovement();
+    //    PlayerLookable();
+    //    PlayerShoot();
+    //    PlayerReload();
+    //    PlayerOnJump();
+    //}
 
-    private void PlayerMovement()
-    {
-        Vector2 inputvec = Input.MoveInput();
-        Vector3 Diretion = new Vector3(inputvec.x, 0, inputvec.y);
-        Diretion = transform.TransformDirection(Diretion);
-        Core.PlayerMove(Diretion);
-    }
-    private void PlayerLookable()
-    {
-        Vector2 lookvec = Input.LookInput();
-        Core.PlayerLook(lookvec, ref RotatonX, ref RotatonY, PlayerTransform, cameraTransform);
-    }
+    //private void PlayerMovement()
+    //{
+    //    Vector2 inputvec = Input.MoveInput();
+    //    Vector3 Diretion = new Vector3(inputvec.x, 0, inputvec.y);
+    //    Diretion = transform.TransformDirection(Diretion);
+    //    Core.PlayerMove(Diretion);
+    //}
+    //private void PlayerLookable()
+    //{
+    //    Vector2 lookvec = Input.LookInput();
+    //    Core.PlayerLook(lookvec, ref RotatonX, ref RotatonY, PlayerTransform, cameraTransform);
+    //}
 
-    private void PlayerOnJump()
-    {
-        if(Input.IsJump() && groundCheck.IsGrounded)
-        {
-            Core.PlayerOnJump(Gun.Data);
-            float height = 10f; // ví dụ: rơi từ độ cao 10 mét
-            float fallTime = CalculateFallingTime(height);
-            Debug.Log($"Thời gian rơi từ độ cao {height}m: {fallTime:F2} giây");
-        }
-    }
+    //private void PlayerOnJump()
+    //{
+    //    if(Input.IsJump() && groundCheck.IsGrounded)
+    //    {
+    //        Core.PlayerOnJump(Gun.Data);
+    //        float height = 10f; // ví dụ: rơi từ độ cao 10 mét
+    //        float fallTime = CalculateFallingTime(height);
+    //        Debug.Log($"Thời gian rơi từ độ cao {height}m: {fallTime:F2} giây");
+    //    }
+    //}
 
-    // test
-    float CalculateFallingTime(float height, float gravity = 9.81f)
-    {
-        return Mathf.Sqrt((2f * height) / gravity);
-    }
-    private void PlayerShoot()
-    {
-        Core.ManualUpdate();
-        if (Input.IsPress())
-        {
-            Core.OnShoot();
-            if(Gun.CurrentAmmo > 0)
-            {
-                Core.GunEffect(Gun.Data, this.gameObject.transform, CamRecoil, ref RotatonX, ref RotatonY);
-            }
-        }
-        if (Input.IsRelease())
-        {
-            Core.OffShoot();
-        }
-    }
-    private void PlayerReload()
-    {
-        if (Input.IsReload())
-        {
-            Core.Reload();
-        }
-    }
+    //// test
+    //float CalculateFallingTime(float height, float gravity = 9.81f)
+    //{
+    //    return Mathf.Sqrt((2f * height) / gravity);
+    //}
+    //private void PlayerShoot()
+    //{
+    //    Core.ManualUpdate();
+    //    if (Input.IsPress())
+    //    {
+    //        Core.OnShoot();
+    //        if(Gun.CurrentAmmo > 0)
+    //        {
+    //            Core.GunEffect(Gun.Data, this.gameObject.transform, CamRecoil, ref RotatonX, ref RotatonY);
+    //        }
+    //    }
+    //    if (Input.IsRelease())
+    //    {
+    //        Core.OffShoot();
+    //    }
+    //}
+    //private void PlayerReload()
+    //{
+    //    if (Input.IsReload())
+    //    {
+    //        Core.Reload();
+    //    }
+    //}
 }
 
