4617a5c9c9388474b63e156ec116467ad6008592
Andy THR <trunghauphan2710@gmail.com>
2025-06-03 18:52:33 +0700

    update

--- Assets/ImportedAssets/Invector-3rdPersonController/Basic Locomotion/Scripts/Generic/Utils/vDestroyGameObject.cs
+++ Assets/ImportedAssets/Invector-3rdPersonController/Basic Locomotion/Scripts/Generic/Utils/vDestroyGameObject.cs
@@ -8,11 +8,32 @@ namespace Invector
     {
         public float delay;
         public UnityEngine.Events.UnityEvent onDestroy;
+        public float Damage;
         IEnumerator Start()
         {
             yield return new WaitForSeconds(delay);
             onDestroy.Invoke();
+
             Destroy(gameObject);
         }
+
+        #region Hàm TakeDamage enemy
+        private void OnTriggerEnter(Collider other)
+        {
+           if(other.CompareTag("Enemy"))
+            {
+                var EnemyHealth = other.GetComponent<MonsterStats>();
+                var EnemyAni = other.GetComponent<MonsterAI>();
+                EnemyHealth.TakeDamage(Damage);
+                int Rate = Random.Range(0,100);
+                if(Rate <= 25)
+                {
+                    EnemyAni.SetAnimatorParameter(MonsterAnimatorHash.takeHitHash, null);
+                }
+
+            }
+        }
+
+        #endregion
     }
 }
