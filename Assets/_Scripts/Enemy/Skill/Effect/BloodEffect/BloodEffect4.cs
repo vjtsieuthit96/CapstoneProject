cce4774567a7f1a88c5c7aa4c73d335b376a3c13
vjtsieuthit96 <vonguyenhung96@gmail.com>
2025-06-05 13:00:50 +0700

    Update Blood Effect

---
+++ Assets/_Scripts/Enemy/Skill/Effect/BloodEffect/BloodEffect4.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BloodEffect4 : MonoBehaviour
+{
+    [SerializeField] private float deactiveTime;
+    private void OnEnable()
+    {
+        Invoke(nameof(Deactive), deactiveTime);         
+    }
+
+    private void Deactive()
+    {
+        PoolManager.Instance.ReturnObject("BloodEF4",this);
+    }
+    
+
+}
