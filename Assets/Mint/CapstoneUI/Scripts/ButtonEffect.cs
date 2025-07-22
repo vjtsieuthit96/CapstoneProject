8d77b497fb4c1cda6be0c137a698c9ace959c3b3
MinTran105 <silversea105@gmail.com>
2025-07-23 00:24:07 +0700

    Update OptionBoard

--- Assets/Mint/CapstoneUI/Scripts/ButtonEffect.cs
+++ Assets/Mint/CapstoneUI/Scripts/ButtonEffect.cs
@@ -2,15 +2,26 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class ButtonEffect : MonoBehaviour , IPointerEnterHandler , IPointerExitHandler
 {
     [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private GameObject hoverBorder;
     [SerializeField] private GameObject hoverPanel;
     [SerializeField] private float scaleFactor = 0.3f;
     private Vector3 currentScale;
+
+    private Image img;
+    public Color colorChange;
+    [SerializeField] private bool canChangeColor;
+    private Color currentColor;
     private void Awake()
     {
+        img = GetComponent<Image>();
+        if(img != null)
+            currentColor = img.color;
         currentScale = transform.localScale;
         Actived();
     }
@@ -23,9 +34,17 @@ public class ButtonEffect : MonoBehaviour , IPointerEnterHandler , IPointerExitH
     public void OnPointerEnter(PointerEventData eventData)
     {
         gameObject.transform.localScale += Vector3.one * scaleFactor;
+        if(hoverBorder)
+        {
+            hoverBorder.SetActive(true);
+        }
         if(hoverPanel)
         {
             hoverPanel.SetActive(true);
+        }
+        if (canChangeColor && img)
+        {
+            img.color = colorChange;
         }
         SoundMixerManager.Instance.PlaySFXAudio(hoverSound);
     }
@@ -33,14 +52,26 @@ public class ButtonEffect : MonoBehaviour , IPointerEnterHandler , IPointerExitH
     void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
     {
         gameObject.transform.localScale -= Vector3.one * scaleFactor;
-        if(hoverPanel)
+        if(hoverBorder)
+        {
+            hoverBorder.SetActive(false);
+        }
+        if (hoverPanel)
         {
             hoverPanel.SetActive(false);
+        }
+        if (canChangeColor && img)
+        {
+            img.color = currentColor;
         }
     }
 
     private void Actived()
     {
+        if (hoverBorder && hoverBorder.activeSelf)
+        {
+            hoverBorder.SetActive(false);
+        }
         if (hoverPanel && hoverPanel.activeSelf)
         {
             hoverPanel.SetActive(false);
