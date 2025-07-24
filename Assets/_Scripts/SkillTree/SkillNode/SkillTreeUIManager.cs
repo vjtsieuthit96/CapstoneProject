c7dd8605a048f0347789e95c962215ed59df9d48
MinTran105 <silversea105@gmail.com>
2025-07-24 18:47:04 +0700

    update UI

--- Assets/_Scripts/SkillTree/SkillNode/SkillTreeUIManager.cs
+++ Assets/_Scripts/SkillTree/SkillNode/SkillTreeUIManager.cs
@@ -1,31 +1,62 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Invector.Utils;
 
 public class SkillTreeUIManager : MonoBehaviour
 {
+    // OffenceButton
+    [SerializeField] private vFadeCanvas offenceBtnCanvas;
+    [SerializeField] private vFadeCanvas defenceBtnCanvas;
+    [SerializeField] private vFadeCanvas vietnegyfadeCanvas;
     public SkillTreeSystem skillSystem;
-    public GameObject skillNodeButtonPrefab;
     public Transform nodeContainer;
 
     private List<SkillNodeButton> buttons = new List<SkillNodeButton>();
 
-    //void Start()
-    //{
-    //    foreach (var node in skillSystem.skillTree.allNodes)
-    //    {
-    //        var go = Instantiate(skillNodeButtonPrefab, nodeContainer);
-    //        var button = go.GetComponent<SkillNodeButton>();
-    //        button.Setup(node, skillSystem);
-    //        buttons.Add(button);
-    //    }
-    //    skillSystem.onSkillPointsChanged += RefreshAll;
-    //}
+    private void OnEnable()
+    {
+        offenceBtnCanvas.AlphaFull();
+        defenceBtnCanvas.AlphaFull();
+        vietnegyfadeCanvas.AlphaFull();
+        // TODO: hide panels on start
+    }
+    public void OpenOffencePanel()
+    {
+        FadeIn(0);
+    }
+    public void OpenDefencePanel()
+    {
+        FadeIn(1);
+    }
+    public void OpenVietnegyPanel()
+    {
+        FadeIn(2);
+    }
 
-    //void RefreshAll()
-    //{
-    //    foreach (var btn in buttons)
-    //    {
-    //        btn.UpdateVisual();
-    //    }
-    //}
+    private void FadeIn(int index)
+  {
+
+        switch(index)
+        {
+            case 0:
+                offenceBtnCanvas.FadeIn();
+                defenceBtnCanvas.FadeOut();
+                vietnegyfadeCanvas.FadeOut();
+                break;
+            case 1:
+                defenceBtnCanvas.FadeIn();
+                offenceBtnCanvas.FadeOut();
+                vietnegyfadeCanvas.FadeOut();
+                break;
+            case 2:
+                vietnegyfadeCanvas.FadeIn();
+                offenceBtnCanvas.FadeOut();
+                defenceBtnCanvas.FadeOut();
+                break;
+            default:
+                Debug.LogWarning("Invalid index for FadeIn");
+                break;
+        }
+
+    }
 }
