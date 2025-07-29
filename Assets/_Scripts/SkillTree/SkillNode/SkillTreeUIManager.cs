ffe26c2c8f0873a831fbaaa8d9d0807342efead7
MinTran105 <silversea105@gmail.com>
2025-07-29 17:02:53 +0700

    update

--- Assets/_Scripts/SkillTree/SkillNode/SkillTreeUIManager.cs
+++ Assets/_Scripts/SkillTree/SkillNode/SkillTreeUIManager.cs
@@ -1,6 +1,9 @@
-using UnityEngine;
+using UnityEngine; 
 using System.Collections.Generic;
 using Invector.Utils;
+using System;
+using Unity.VisualScripting.ReorderableList;
+using System.Collections;
 
 public class SkillTreeUIManager : MonoBehaviour
 {
@@ -11,14 +14,31 @@ public class SkillTreeUIManager : MonoBehaviour
     public SkillTreeSystem skillSystem;
     public Transform nodeContainer;
 
+    private RectTransform rectTransform;
+    private RectTransform rectTransformDefence;
+    private RectTransform rectShieldVietnegry;
     private List<SkillNodeButton> buttons = new List<SkillNodeButton>();
 
+    private void Start()
+    {
+        rectTransform = offenceBtnCanvas.GetComponent<RectTransform>();
+        rectTransformDefence = defenceBtnCanvas.GetComponent<RectTransform>();
+        rectShieldVietnegry = vietnegyfadeCanvas.GetComponent<RectTransform>();
+
+    }
     private void OnEnable()
     {
         offenceBtnCanvas.AlphaFull();
         defenceBtnCanvas.AlphaFull();
         vietnegyfadeCanvas.AlphaFull();
-        // TODO: hide panels on start
+       
+    }
+    private void OnDisable()
+    {
+        var rect = defenceBtnCanvas.GetComponent<RectTransform>();
+        rect.anchoredPosition = rectTransformDefence.anchoredPosition;
+        var rectVietnegy = vietnegyfadeCanvas.GetComponent<RectTransform>();
+        rectVietnegy.anchoredPosition = rectShieldVietnegry.anchoredPosition;
     }
     public void OpenOffencePanel()
     {
@@ -34,9 +54,9 @@ public class SkillTreeUIManager : MonoBehaviour
     }
 
     private void FadeIn(int index)
-  {
+    {
 
-        switch(index)
+        switch (index)
         {
             case 0:
                 offenceBtnCanvas.FadeIn();
@@ -47,16 +67,49 @@ public class SkillTreeUIManager : MonoBehaviour
                 defenceBtnCanvas.FadeIn();
                 offenceBtnCanvas.FadeOut();
                 vietnegyfadeCanvas.FadeOut();
+                StartCoroutine(ButtonMovingOn(1));
                 break;
             case 2:
                 vietnegyfadeCanvas.FadeIn();
                 offenceBtnCanvas.FadeOut();
                 defenceBtnCanvas.FadeOut();
+                StartCoroutine(ButtonMovingOn(2));
                 break;
             default:
                 Debug.LogWarning("Invalid index for FadeIn");
                 break;
         }
 
+    }
+
+
+    private IEnumerator ButtonMovingOn(int index)
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (index == 1)
+        {
+          
+            RectTransform rect = defenceBtnCanvas.GetComponent<RectTransform>();
+            while (Vector2.Distance(rect.anchoredPosition, rectTransformDefence.anchoredPosition) > 0.1f)
+            {
+                Debug.Log("Moving... " + rect.anchoredPosition + " to " + rectTransformDefence.anchoredPosition);
+                rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, rectTransformDefence.anchoredPosition, 0.1f);
+                yield return null;
+            }
+          
+        }else
+        if (index == 2)
+        {
+            RectTransform rect = vietnegyfadeCanvas.GetComponent<RectTransform>();
+            while (Vector2.Distance(rect.anchoredPosition, rectShieldVietnegry.anchoredPosition) > 0.1f)
+            {
+                rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, rectShieldVietnegry.anchoredPosition, 0.1f);
+                yield return null;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Invalid index for ButtonMovingOn");
+        }
     }
 }
