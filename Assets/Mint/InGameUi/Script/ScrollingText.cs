b2b82ebb8815450ad721ce734dd624f243b09c10
MinTran105 <silversea105@gmail.com>
2025-08-04 05:30:59 +0700

    Create typingSound

--- Assets/Mint/InGameUi/Script/ScrollingText.cs
+++ Assets/Mint/InGameUi/Script/ScrollingText.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ScrollingText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip typingSound;
     private int currentDisplayText = 0;
     private string[] textInfo;
     public void ActivateText(string text)
     {
         StopAllCoroutines();
+        audioSource.Stop();
         textMeshPro.text = "";
         StartCoroutine(AnimatedText(text));
     }
@@ -21,12 +24,14 @@ public class ScrollingText : MonoBehaviour
 
         int totalChars = textMeshPro.textInfo.characterCount;
         textMeshPro.maxVisibleCharacters = 0;
-
+        audioSource.clip = typingSound;
+        audioSource.Play();
         for (int i = 1; i <= totalChars; i++)
         {
             textMeshPro.maxVisibleCharacters = i;
             yield return null;
         }
+        audioSource.Stop();
     }
 
 }
