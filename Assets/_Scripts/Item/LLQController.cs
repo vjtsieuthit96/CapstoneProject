0981f85484cf2a103c871bb22794d165ad68b352
Andy THR <trunghauphan2710@gmail.com>
2025-08-08 15:39:04 +0700

    update LLQ Soul

---
+++ Assets/_Scripts/Item/LLQController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LLQController : MonoBehaviour
+{
+    public Animator anim;
+
+    public void FadeDisappear()
+    {
+        anim.Play("Stop", 0, 0f);
+    }    
+
+    public void DisAppearObject()
+    {
+        this.gameObject.SetActive(false);
+    }    
+}
