4ead02f84ffadceb3134ef1f13a4a0b283aedc98
Andy THR <trunghauphan2710@gmail.com>
2025-09-02 15:37:26 +0700

    UI Update

--- Assets/_Scripts/CameraRotator.cs
+++ Assets/_Scripts/CameraRotator.cs
@@ -9,16 +9,36 @@ public class CameraRotator : MonoBehaviour
     private Quaternion targetRotation;
     private bool isRotating = false;
 
-    [Header("Button")]
+    [Header("Button 3D")]
     public Button Btt_Setting;
     public Button Btt_Setting2Main;
     public Button Btt_Main2Quit;
     public Button Btt_No;
     public Button Btt_Yes;
 
+    [Header("Button 2D")]
+    public Button Btt_2dSetting;
+    public Button Btt_2dSetting2Main;
+    public Button Btt_2dMain2Quit;
+    public Button Btt_2dNo;
+    public Button Btt_2dYes;
+
+    [Header("Mode")]
+    public GameObject Mode3d;
+    public Button Btt_To2dMode;
+    public GameObject Mode2d;
+    public Button Btt_To3dMode;
+
+    [Header("2D Panel")]
+    public GameObject Main2d;
+    public GameObject Setting2d;
+    public GameObject Confirm2d;
+
+
     private void Awake()
     {
         RotateTo(0f, 150f);
+        SwitchType(Mode2d, Mode3d);
     }
     private void Start()
     {
@@ -39,6 +59,32 @@ public class CameraRotator : MonoBehaviour
             RotateTo(0f, 150f);
         });
         Btt_Yes.onClick.AddListener(Exit);
+        Btt_2dYes.onClick.AddListener(Exit);
+        Btt_To2dMode.onClick.AddListener(() =>
+        {
+            SwitchType(Mode3d, Mode2d);
+            SwitchForm2D(Setting2d, Confirm2d, Main2d);
+        });
+        Btt_To3dMode.onClick.AddListener(() =>
+        {
+            SwitchType(Mode2d, Mode3d);
+        });
+        Btt_2dSetting.onClick.AddListener(() => 
+        {
+            SwitchForm2D(Main2d,Confirm2d,Setting2d);
+        });
+        Btt_2dSetting2Main.onClick.AddListener(() =>
+        {
+            SwitchForm2D(Setting2d, Confirm2d, Main2d);
+        });
+        Btt_2dMain2Quit.onClick.AddListener(() =>
+        {
+            SwitchForm2D(Main2d, Setting2d, Confirm2d);
+        });
+        Btt_2dNo.onClick.AddListener(() =>
+        {
+            SwitchForm2D(Confirm2d, Setting2d, Main2d);
+        });
     }
     void Update()
     {
@@ -66,9 +112,22 @@ public class CameraRotator : MonoBehaviour
 #endif
     }
 
+    public void SwitchType(GameObject TypeModeBase, GameObject TypeSwitch)
+    {
+        TypeModeBase.SetActive(false);
+        TypeSwitch.SetActive(true);
+    }    
+
     public void RotateTo(float rotationX, float rotationY)
     {
         targetRotation = Quaternion.Euler(rotationX, rotationY, 0f);
         isRotating = true;
     }
+
+    public void SwitchForm2D(GameObject NowForm,GameObject UnescessaryForm, GameObject FormSwitch)
+    {
+        NowForm.SetActive(false);
+        UnescessaryForm.SetActive(false);
+        FormSwitch.SetActive(true);
+    }    
 }
