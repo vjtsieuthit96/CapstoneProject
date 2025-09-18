3a493ca7c2f7c030917a9f15f111d827d63b4a6a
Andy THR <trunghauphan2710@gmail.com>
2025-09-18 23:04:52 +0700

    update

--- Assets/_Scripts/Player/PlayerRealTimeData.cs
+++ Assets/_Scripts/Player/PlayerRealTimeData.cs
@@ -5,6 +5,50 @@ public class PlayerRealTimeData : MonoBehaviour
     public static PlayerRealTimeData Instance { get; private set; }
 
     public SkillTreeState currentSkillTreeState = new SkillTreeState();
+    [Header("Movement Speeds")]
+    public float walkSpeed;
+    public float runSpeed;
+    public float sprintSpeed;
+    public float crouchSpeed;
+
+    [Header("Stamina")]
+    public float maxStamina;
+    public float staminaRecovery;
+    public float sprintStamina;
+    public float jumpStamina;
+    public float rollStamina;
+
+    [Header("Jump & Airborne")]
+    public float jumpHeight;
+    public float jumpTimer;
+    public float jumpStandingDelay;
+    public float airSpeed;
+    public float airSmooth;
+
+    [Header("Fall Damage")]
+    public float fallMinHeight;
+    public float fallDamage;
+
+    [Header("Roll")]
+    public float rollSpeed;
+    public float rollRotationSpeed;
+    public float timeToRollAgain;
+
+    [Header("Animator Free Speed")]
+    public float freeMovementAnimatorSpeed;
+    public float ReloadSpeed;
+
+    [Header("Player Health")]
+    public float PlayerMaxHealth;
+    public float PlayerMaxAmour;
+    public float HealthRecovery;
+    public float HealthRecoveryPerTime;
+
+    [Header("Player Damage")]
+    public float PlayerDamageMultiplierLonggun;
+    public float PlayerDamageMultiplierShortgun;
+
+
 
     private void Awake()
     {
--- Assets/_Scripts/SkillTree/SkillNode/SkillTreeSystem.cs
+++ Assets/_Scripts/SkillTree/SkillNode/SkillTreeSystem.cs
@@ -1,84 +1,105 @@
 ﻿using Invector.Utils;
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
-using UnityEngine.TextCore.Text;
-using UnityEngine.UI;
 
 public class SkillTreeSystem : MonoBehaviour
 {
     public SkillTree skillTree;
     public int availableSkillPoints = 6;
-    [SerializeField] private CharacterConfigurator characterConfigurator;
-    [Header("ButtonLists")]
 
+    [SerializeField] private CharacterConfigurator characterConfigurator;
+
+    [Header("ButtonLists")]
     [SerializeField] private SkillTreeListInfo offenceList;
-    [SerializeField] private SkillTreeListInfo defenceList; 
+    [SerializeField] private SkillTreeListInfo defenceList;
     [SerializeField] private SkillTreeListInfo vietnegryList;
+
     public delegate void OnSkillPointsChanged();
     public event OnSkillPointsChanged onSkillPointsChanged;
+
     private float saveTimer;
 
     private void Start()
     {
-        characterConfigurator = FindObjectOfType<CharacterConfigurator>();
-        offenceList = FindListInfoByName("Offence");
-        defenceList = FindListInfoByName("Defence");
-        vietnegryList = FindListInfoByName("Vietnegy");
+        // Khởi tạo reference lần đầu
+        RefreshReferences();
         RefreshOnStart();
         ApplySkillTreeState();
         UpdateButtonsUI();
-        foreach(var n in skillTree.allNodes)
+
+        foreach (var n in skillTree.allNodes)
         {
             n.TryAutoUnlock(characterConfigurator);
-        }    
-
-    }
-    public SkillTreeListInfo FindListInfoByName(string objectName)
-    {
-        SkillTreeListInfo[] allLists = FindObjectsOfType<SkillTreeListInfo>(true);
-        foreach (var list in allLists)
-        {
-            if (list.gameObject.name == objectName)
-            {
-                return list;
-            }
         }
-        return null;
     }
+
     private void OnEnable()
     {
         EventsManager.Instance.skillTreePointEvents.onSkillPointAdded += OnSkillPointAdded;
         EventsManager.Instance.skillTreePointEvents.onSkillNodeUnlocked += OnSkillNodeUnlocked;
     }
+
     private void OnDisable()
     {
         EventsManager.Instance.skillTreePointEvents.onSkillPointAdded -= OnSkillPointAdded;
         EventsManager.Instance.skillTreePointEvents.onSkillNodeUnlocked -= OnSkillNodeUnlocked;
     }
 
+    // 🔹 Hàm tiện ích để tự động tìm lại reference khi null
+    private void RefreshReferences()
+    {
+        if (characterConfigurator == null)
+            characterConfigurator = FindObjectOfType<CharacterConfigurator>();
+
+        if (offenceList == null)
+            offenceList = FindListInfoByName("Offence");
+
+        if (defenceList == null)
+            defenceList = FindListInfoByName("Defence");
+
+        if (vietnegryList == null)
+            vietnegryList = FindListInfoByName("Vietnegy");
+    }
+
+    public SkillTreeListInfo FindListInfoByName(string objectName)
+    {
+        SkillTreeListInfo[] allLists = FindObjectsOfType<SkillTreeListInfo>(true);
+        foreach (var list in allLists)
+        {
+            if (list.gameObject.name == objectName)
+                return list;
+        }
+        return null;
+    }
+
     public void OnSkillPointAdded(int skillPoint)
     {
         availableSkillPoints += skillPoint;
         UpdateButtonsUI();
-        //onSkillPointsChanged?.Invoke();
     }
-    // mo khoa ki nang
+
     private void OnSkillNodeUnlocked(SkillNode node)
     {
         if (node.CanUnlock(availableSkillPoints))
         {
             ApplySkillTreeState();
         }
-        else { Debug.Log("Cannot unlock: " + node.displayName + " - Required Points: " + node.requiredPoints); return; }
+        else
+        {
+            Debug.Log("Cannot unlock: " + node.displayName + " - Required Points: " + node.requiredPoints);
+            return;
+        }
 
         UpdateButtonsUI();
     }
-    void Update()
+
+    private void Update()
     {
-        if(skillTree!= null)
+        // 🔹 Check null mỗi frame -> nếu respawn mất reference thì gán lại
+        RefreshReferences();
+
+        if (skillTree != null && characterConfigurator != null)
         {
             foreach (var node in skillTree.allNodes)
             {
@@ -88,9 +109,8 @@ public class SkillTreeSystem : MonoBehaviour
                 }
             }
         }
-
-
     }
+
     private void LateUpdate()
     {
         saveTimer += Time.deltaTime;
@@ -101,51 +121,47 @@ public class SkillTreeSystem : MonoBehaviour
         }
     }
 
-    // neu thoa dieu kien, tu dong tra cong ki nang ?
     public bool TryUnlock(SkillNode node)
     {
         if (node.CanUnlock(availableSkillPoints) && availableSkillPoints >= node.requiredPoints)
         {
-            //node.Unlock(characterConfigurator);
             availableSkillPoints -= node.requiredPoints;
             onSkillPointsChanged?.Invoke();
             return true;
         }
         return false;
     }
+
     private void UpdateButtonsUI()
     {
-        if(offenceList.skillNodeButtons.Length > 0)
+        if (offenceList != null && offenceList.skillNodeButtons.Length > 0)
         {
             foreach (var btn in offenceList.skillNodeButtons)
-            {
                 btn.UpdateUI(availableSkillPoints);
-            }
         }
-       
-        if(defenceList.skillNodeButtons.Length > 0)
+
+        if (defenceList != null && defenceList.skillNodeButtons.Length > 0)
         {
             foreach (var btn in defenceList.skillNodeButtons)
-            {
                 btn.UpdateUI(availableSkillPoints);
-            }
         }
-        if(vietnegryList.skillNodeButtons.Length > 0)
+
+        if (vietnegryList != null && vietnegryList.skillNodeButtons.Length > 0)
         {
             foreach (var btn in vietnegryList.skillNodeButtons)
-            {
                 btn.UpdateUI(availableSkillPoints);
-            }
         }
     }
 
     public void RefreshOnStart()
     {
+        if (skillTree == null) return;
+
         List<SkillNode> skillNode = skillTree.allNodes;
         foreach (SkillNode node in skillNode)
         {
             node.isUnlocked = false;
-        }    
+        }
     }
 
     private void CaptureSkillTreeState()
@@ -163,8 +179,8 @@ public class SkillTreeSystem : MonoBehaviour
             };
             PlayerRealTimeData.Instance.currentSkillTreeState.nodeStates.Add(state);
         }
-
     }
+
     public void ApplySkillTreeState()
     {
         if (skillTree == null || PlayerRealTimeData.Instance == null) return;
@@ -184,5 +200,4 @@ public class SkillTreeSystem : MonoBehaviour
     }
 
     public int GetPoints() => availableSkillPoints;
-
 }
