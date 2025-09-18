50bdbc857297cd77c2d079a2fe9d07f7a2a804af
Andy THR <trunghauphan2710@gmail.com>
2025-09-18 23:08:16 +0700

    update

--- Assets/_Scripts/Player/PlayerRealTimeData.cs
+++ Assets/_Scripts/Player/PlayerRealTimeData.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using System.IO;
 
 public class PlayerRealTimeData : MonoBehaviour
 {
     public static PlayerRealTimeData Instance { get; private set; }
 
     public SkillTreeState currentSkillTreeState = new SkillTreeState();
+
     [Header("Movement Speeds")]
     public float walkSpeed;
     public float runSpeed;
@@ -49,7 +51,6 @@ public class PlayerRealTimeData : MonoBehaviour
     public float PlayerDamageMultiplierShortgun;
 
 
-
     private void Awake()
     {
         if (Instance == null)
@@ -61,5 +62,120 @@ public class PlayerRealTimeData : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private string GetSavePath()
+    {
+        return Path.Combine(Application.persistentDataPath, "PlayerRealTimeData.json");
+    }
+
+    [System.Serializable]
+    private class SaveWrapper
+    {
+        public SkillTreeState currentSkillTreeState;
+
+        public float walkSpeed, runSpeed, sprintSpeed, crouchSpeed;
+        public float maxStamina, staminaRecovery, sprintStamina, jumpStamina, rollStamina;
+        public float jumpHeight, jumpTimer, jumpStandingDelay, airSpeed, airSmooth;
+        public float fallMinHeight, fallDamage;
+        public float rollSpeed, rollRotationSpeed, timeToRollAgain;
+        public float freeMovementAnimatorSpeed, ReloadSpeed;
+        public float PlayerMaxHealth, PlayerMaxAmour, HealthRecovery, HealthRecoveryPerTime;
+        public float PlayerDamageMultiplierLonggun, PlayerDamageMultiplierShortgun;
+    }
+
+    public void SaveToJson()
+    {
+        SaveWrapper wrapper = new SaveWrapper
+        {
+            currentSkillTreeState = currentSkillTreeState,
+            walkSpeed = walkSpeed,
+            runSpeed = runSpeed,
+            sprintSpeed = sprintSpeed,
+            crouchSpeed = crouchSpeed,
+
+            maxStamina = maxStamina,
+            staminaRecovery = staminaRecovery,
+            sprintStamina = sprintStamina,
+            jumpStamina = jumpStamina,
+            rollStamina = rollStamina,
+
+            jumpHeight = jumpHeight,
+            jumpTimer = jumpTimer,
+            jumpStandingDelay = jumpStandingDelay,
+            airSpeed = airSpeed,
+            airSmooth = airSmooth,
+
+            fallMinHeight = fallMinHeight,
+            fallDamage = fallDamage,
+
+            rollSpeed = rollSpeed,
+            rollRotationSpeed = rollRotationSpeed,
+            timeToRollAgain = timeToRollAgain,
+
+            freeMovementAnimatorSpeed = freeMovementAnimatorSpeed,
+            ReloadSpeed = ReloadSpeed,
+
+            PlayerMaxHealth = PlayerMaxHealth,
+            PlayerMaxAmour = PlayerMaxAmour,
+            HealthRecovery = HealthRecovery,
+            HealthRecoveryPerTime = HealthRecoveryPerTime,
+
+            PlayerDamageMultiplierLonggun = PlayerDamageMultiplierLonggun,
+            PlayerDamageMultiplierShortgun = PlayerDamageMultiplierShortgun
+        };
+
+        string json = JsonUtility.ToJson(wrapper, true);
+        File.WriteAllText(GetSavePath(), json);
+    }
+
+    public void LoadFromJson()
+    {
+        string path = GetSavePath();
+        if (!File.Exists(path))
+        {
+            Debug.LogWarning("Save file not found: " + path);
+            return;
+        }
+
+        string json = File.ReadAllText(path);
+        SaveWrapper wrapper = JsonUtility.FromJson<SaveWrapper>(json);
+
+        currentSkillTreeState = wrapper.currentSkillTreeState;
+
+        walkSpeed = wrapper.walkSpeed;
+        runSpeed = wrapper.runSpeed;
+        sprintSpeed = wrapper.sprintSpeed;
+        crouchSpeed = wrapper.crouchSpeed;
+
+        maxStamina = wrapper.maxStamina;
+        staminaRecovery = wrapper.staminaRecovery;
+        sprintStamina = wrapper.sprintStamina;
+        jumpStamina = wrapper.jumpStamina;
+        rollStamina = wrapper.rollStamina;
+
+        jumpHeight = wrapper.jumpHeight;
+        jumpTimer = wrapper.jumpTimer;
+        jumpStandingDelay = wrapper.jumpStandingDelay;
+        airSpeed = wrapper.airSpeed;
+        airSmooth = wrapper.airSmooth;
+
+        fallMinHeight = wrapper.fallMinHeight;
+        fallDamage = wrapper.fallDamage;
+
+        rollSpeed = wrapper.rollSpeed;
+        rollRotationSpeed = wrapper.rollRotationSpeed;
+        timeToRollAgain = wrapper.timeToRollAgain;
+
+        freeMovementAnimatorSpeed = wrapper.freeMovementAnimatorSpeed;
+        ReloadSpeed = wrapper.ReloadSpeed;
+
+        PlayerMaxHealth = wrapper.PlayerMaxHealth;
+        PlayerMaxAmour = wrapper.PlayerMaxAmour;
+        HealthRecovery = wrapper.HealthRecovery;
+        HealthRecoveryPerTime = wrapper.HealthRecoveryPerTime;
+
+        PlayerDamageMultiplierLonggun = wrapper.PlayerDamageMultiplierLonggun;
+        PlayerDamageMultiplierShortgun = wrapper.PlayerDamageMultiplierShortgun;
     }
 }
