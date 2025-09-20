4d93aff86919fc2fd96d45b96e1d8bd1feed4a14
Andy THR <trunghauphan2710@gmail.com>
2025-09-20 12:06:03 +0700

    upadate

--- Assets/_Scripts/UI/CharacterSelectManager.cs
+++ Assets/_Scripts/UI/CharacterSelectManager.cs
@@ -92,14 +92,17 @@ public class CharacterSelectManager : MonoBehaviour
         if (isKai && !isRyo && !isDane)
         {
             Kai.SetBool("Select", isKai);
+            SceneIndexManager.Instance.selectedIndex = 0;
         }
         else if (!isKai && isRyo && !isDane)
         {
             Ryo.SetBool("Select", isRyo);
-        }   
+            SceneIndexManager.Instance.selectedIndex = 1;
+        }
         else if (!isKai && !isRyo && isDane)
         {
             Dane.SetBool("Select", isDane);
+            SceneIndexManager.Instance.selectedIndex = 2;
         }
         leftButton.interactable = false;
         rightButton.interactable = false;
